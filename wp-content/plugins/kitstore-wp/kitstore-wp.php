42d59fc397ba2d82d11c53ba4fa81656ed7168eb
Richard Shelswell <rshelswell@users.noreply.github.com>
2022-04-18 10:33:59 +0100

    kitstore-wp creation
    First header file for plugin and initial directory layout

---
+++ wp-content/plugins/kitstore-wp/kitstore-wp.php
@@ -0,0 +1,10 @@
+/**
+ * Plugin Name: Kitstore WP
+ * Plugin URI: richardshelswell.co.uk/kitstore-wp
+ * Version: 1.0
+ * Description: Inventory system plugin for Wordpress. Barcode tracking of items, sign kit out to a user, and back in to store. Keeps track of repairs needed, blocking loans if necessary. View kit by type, availability and user.
+ * Author: Rich Shelswell
+ * Author URI: http://www.richardshelswell.co.uk
+ * License: Apache-2.0
+ * License URI: http://www.apache.org/licenses/LICENSE-2.0
+**/
